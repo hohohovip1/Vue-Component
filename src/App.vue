bf3cfeb3d07d9b3b25c90b0b0f214b9756ab6ec1
Hoangtt <ta.tuan.hoang@vsi-international.com>
2021-07-20 15:23:03 +0700

    New

--- src/App.vue
+++ src/App.vue
@@ -4,65 +4,111 @@
         <h1>{{ title }}</h1>
         <p>Welcome to gatcha hell ....</p>
         <h3>Vue in training</h3>
-        <div class="p-grid">
-            <div
-                class="p-col-12 p-md-4"
-                v-for="item in trainingList"
-                :key="item.id"
-            >
-                <div class="p-field-radiobutton">
-                    <RadioButton
-                        :id="item.id"
-                        name="todo"
-                        :value="item.text"
-                        v-model="title"
-                        :key="item.id"
-                    />
-                    <label :for="item.id">{{ item.text }}</label>
-                </div>
-            </div>
+
+        <div class="p-d-flex">
+            <Button label="Select to do" @click="toggleModal" class="p-mr-2" />
+
+            <Button
+                label="Add more to do"
+                @click="toggleModalTwo"
+                class="p-mr-2"
+            />
         </div>
         <teleport to=".modals" v-if="showModal">
             <CustomModal theme="" @close="toggleModal">
-                <template v-slot:links>
-                    <a href="#">Sign up Now!!!</a>
-                    <a href="#">more info</a>
+                <template v-slot:body>
+                    <div class="select-to-do">
+                        <div
+                            class=" p-md-4"
+                            v-for="item in trainingList"
+                            :key="item.id"
+                        >
+                            <div class="p-field-radiobutton">
+                                <RadioButton
+                                    :id="item.id"
+                                    name="todo"
+                                    :value="item.text"
+                                    v-model="title"
+                                    :key="item.id"
+                                    :load="console.log(item.text)"
+                                    class="p-mr-2 "
+                                />
+                                <label :for="item.id">{{ item.text }}</label>
+                            </div>
+                        </div>
+                    </div>
                 </template>
-                <h1>Primogem giveaway!!!</h1>
+                <template v-slot:footer>
+                    <Button
+                        label="Add more!!"
+                        @click="goToAdd"
+                        class="p-button-success p-mr-2"
+                    />
+                    <Button
+                        label="Close"
+                        @click="toggleModal"
+                        class="p-button-danger"
+                    />
+                </template>
+                <h1>Select something to do!!</h1>
                 <p>Ohh Yehhh</p>
             </CustomModal>
         </teleport>
-        <button @click.alt="toggleModal">Open portal (alt)</button>
 
         <teleport to=".modals" v-if="showModalTwo">
             <CustomModal theme="" @close="toggleModalTwo">
-                <template v-slot:links>
-                    <a href="#">More to come</a>
+                <template v-slot:body>
+                    <span class="p-float-label">
+                        <InputText
+                            id="toDo"
+                            type="text"
+                            v-model="value"
+                            :class="{
+                                'p-inputtext-lg': true,
+                                'p-invalid': error,
+                            }"
+                        />
+                        <label for="toDo">To do</label>
+                        <small
+                            class="p-error"
+                            v-show="error"
+                            style="display: block"
+                            >To do is required</small
+                        >
+                    </span>
                 </template>
-                <h1>Something</h1>
+                <template v-slot:footer>
+                    <Button
+                        label="Add more!!"
+                        @click="addTodo"
+                        class="p-button-success p-mr-2"
+                    />
+                    <Button label="More info" @click="goToAdd" />
+                </template>
+                <h1>Add to do</h1>
             </CustomModal>
         </teleport>
-        <button @click="toggleModalTwo">Open portal</button>
     </div>
 </template>
 
 <script>
 import CustomModal from './components/CustomModal.vue';
 import RadioButton from 'primevue/radiobutton';
+import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
-import Calendar from 'primevue/calendar';
-
 export default {
     name: 'App',
     components: {
         CustomModal,
         RadioButton,
+        Button,
         InputText,
-        Calendar,
     },
 
     data() {
         return {
+            console,
+            error: null,
             value: '',
             title: '',
             showModal: false,
@@ -79,12 +125,37 @@ export default {
             ],
         };
     },
+    created() {
+        this.console = window.console;
+    },
     methods: {
+        goToAdd() {
+            this.toggleModal();
+            this.toggleModalTwo();
+        },
+        addTodo() {
+            console.log(this.value);
+            if (this.value) {
+                const newToDo = {
+                    id: this.trainingList.length,
+                    text: this.value,
+                };
+                this.trainingList = [...this.trainingList, newToDo];
+                this.error = false;
+                this.value = '';
+            } else {
+                console.log('error');
+                this.error = true;
+            }
+        },
         toggleModal() {
             this.showModal = !this.showModal;
         },
         toggleModalTwo() {
             this.showModalTwo = !this.showModalTwo;
+        },
+        log(item) {
+            console.log(item);
         },
     },
 };
@@ -104,5 +175,23 @@ h1 {
     border-bottom: 1px solid #ddd;
     display: inline-block;
     padding-bottom: 10px;
+}
+.p-mr-2 {
+    margin-right: 0.5rem;
+}
+.p-md-4 {
+    margin-bottom: 1rem;
+}
+.select-to-do {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+}
+.p-float-label {
+    width: 100%;
+    margin-bottom: 2rem;
+}
+.p-float-label label {
+    left: 4rem;
 }
 </style>
